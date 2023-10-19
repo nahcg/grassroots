b56a777f8050186404dc78730f37e16e21067798
Swathi Jayasree <swathij943@gmail.com>
2023-10-19 10:28:08 -0400

    Setting up SQL queries & seeding fake data

---
+++ backend/db/seeds/02_communities.sql
@@ -0,0 +1,12 @@
+INSERT INTO Communities (Name, Description, Location, CreationDate)
+VALUES
+    ('Community A', 'Description for Community A', 'Location A', '2023-10-20'),
+    ('Community B', 'Description for Community B', 'Location B', '2023-10-21'),
+    ('Community C', 'Description for Community C', 'Location C', '2023-10-22'),
+    ('Community D', 'Description for Community D', 'Location D', '2023-10-23'),
+    ('Community E', 'Description for Community E', 'Location E', '2023-10-24'),
+    ('Community F', 'Description for Community F', 'Location F', '2023-10-25'),
+    ('Community G', 'Description for Community G', 'Location G', '2023-10-26'),
+    ('Community H', 'Description for Community H', 'Location H', '2023-10-27'),
+    ('Community I', 'Description for Community I', 'Location I', '2023-10-28'),
+    ('Community J', 'Description for Community J', 'Location J', '2023-10-29');
