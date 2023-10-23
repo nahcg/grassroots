95835cc73235ea7778e7623f17e9bd1c1f0948b6
nahcg <glor.chan@mail.utoronto.ca>
2023-10-23 18:10:24 -0400

    change Calendar to include event tiles

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -6,24 +6,50 @@ import '../styles/Calendar.css';
 const CalendarApp = () => {
   const [date, setDate] = useState(new Date());
   const [events, setEvents] = useState([]);
-  const [eventText, setEventText] = useState('');
+  const [eventTitle, setEventTitle] = useState('');
+  const [eventDetails, setEventDetails] = useState('');
 
   const handleDateChange = (newDate) => {
     setDate(newDate);
   };
 
-  const handleEventInputChange = (e) => {
-    setEventText(e.target.value);
-  };
 
   const addEvent = () => {
-    if (eventText.trim() === '') return;
+    if (eventTitle.trim() === '' || eventDetails.trim() === '') return;
     const newEvent = {
       date: date,
-      text: eventText,
+      title: eventTitle,
+      details: eventDetails,
     };
     setEvents([...events, newEvent]);
-    setEventText('');
+    setEventTitle('');
+    setEventDetails('');
+  };
+
+  // Function to determine whether a date has events and return the event texts
+  const tileContent = ({ date, view }) => {
+    if (view === 'month') {
+      const matchingEvents = events.filter((event) => {
+        return (
+          event.date.getDate() === date.getDate() &&
+          event.date.getMonth() === date.getMonth() &&
+          event.date.getFullYear() === date.getFullYear()
+        );
+      });
+  
+      if (matchingEvents.length > 0) {
+        return (
+          <ul>
+            {matchingEvents.map((event, index) => (
+              <li key={index}>
+                {event.title}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+    return null;
   };
 
   return (
@@ -31,23 +57,35 @@ const CalendarApp = () => {
       <h1>Event Calendar</h1>
       <div className="calendar-container">
         <div className="event-form">
-          <input
-            type="text"
-            placeholder="Enter event..."
-            value={eventText}
-            onChange={handleEventInputChange}
-          />
-          <button onClick={addEvent}>Add Event</button>
-        </div>
-        <Calendar onChange={handleDateChange} value={date} />
+  <input
+    type="text"
+    placeholder="Event Title..."
+    value={eventTitle}
+    onChange={(e) => setEventTitle(e.target.value)}
+  />
+  <textarea
+    placeholder="Event Details..."
+    value={eventDetails}
+    onChange={(e) => setEventDetails(e.target.value)}>
+    </textarea>
+  <button onClick={addEvent}>Add Event</button>
+</div>
+        
+        <Calendar
+          onChange={handleDateChange}
+          value={date}
+          tileContent={tileContent}
+        />
         <div className="events">
           <h2>Events:</h2>
           <ul>
-            {events.map((event, index) => (
-              <li key={index}>
-                {event.date.toDateString()} - {event.text}
-              </li>
-            ))}
+          <ul>
+  {events.map((event, index) => (
+    <li key={index}>
+      {event.date.toDateString()} - {event.title} - {event.details}
+    </li>
+  ))}
+</ul>
           </ul>
         </div>
       </div>
