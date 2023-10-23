14c9b7941bbc55b1b100aed765c701115212ee71
nahcg <glor.chan@mail.utoronto.ca>
2023-10-23 00:08:04 -0400

    merge Auth0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,8 @@
-<<<<<<< HEAD
-import "./App.css";
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./views/Home";
-import Community from "./views/Community";
-import Event from "./views/Event";
-import Login from "./views/Login";
-import Register from "./views/Register";
-import Volunteer from "./views/Volunteer";
-
-import Navbar from "./components/Navbar";
-
-function App() {
-  return (
-    <div className="wrapper">
-      <h1>Grassroots</h1>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/volunteer" element={<Volunteer />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
-=======
 import logo from "./logo.svg";
 import "./App.css";
 
 import React, { Component } from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import LoginButton from "./components/login";
 
 class App extends Component {
@@ -65,4 +32,3 @@ class App extends Component {
 }
 
 export default App;
->>>>>>> refs/remotes/origin/master
