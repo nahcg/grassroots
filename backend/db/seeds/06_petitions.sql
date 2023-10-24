0d04e3082cb82fd212a11070d11ee2cca60a71d2
Swathi Jayasree <swathij943@gmail.com>
2023-10-23 20:45:27 -0400

    removing resources & petitions - updating seeds

--- backend/db/seeds/06_petitions.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Petitions (UserID, CommunityID, Title, Description, SignatureCount, Deadline)
-VALUES
-  (1, 1, 'Petition in Community A', 'Petition description for Community A', 50, '2023-03-01'),
-  (2, 2, 'Petition in Community B', 'Petition description for Community B', 75, '2023-03-20');
--- backend/db/seeds/07_resources.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Resources (UserID, Name, Category, Description, Location, ContactInformation)
-VALUES
-  (1, 'Resource A', 'Category A', 'Description for Resource A', 'Location A', 'Contact A'),
-  (2, 'Resource B', 'Category B', 'Description for Resource B', 'Location B', 'Contact B');
