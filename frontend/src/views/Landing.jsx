f97ec81d7b8e94551712a85f0d8e67edaf9b96e1
Swathi Jayasree <swathij943@gmail.com>
2023-10-24 23:26:36 -0400

    Updating landing page

--- frontend/src/views/Landing.jsx
+++ frontend/src/views/Landing.jsx
@@ -5,6 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Landing = () => {
   const { isAuthenticated } = useAuth0(); // Check if the user is authenticated
 
+
   return (
     <div>
       <h1>Welcome to Grassroots App</h1>
@@ -19,6 +20,7 @@ const Landing = () => {
           </Link>
         </div>
       )}
+      
       {/* "Join Communities" button */}
       <Link to={isAuthenticated ? '/community' : '/login'}>
         <button>Join Communities</button>
