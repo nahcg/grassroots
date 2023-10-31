6c9383968c6d26d88383373e4cf21f97c85ac322
nahcg <glor.chan@mail.utoronto.ca>
2023-10-31 18:52:05 -0400

    merge db

--- backend/db/seeds/05_events.sql
+++ backend/db/seeds/05_events.sql
@@ -1,4 +1,8 @@
 INSERT INTO events (user_id, community_id, title, description, datetime, location)
 VALUES
   ('smpere7@gmail.com', 1, 'Event in Community A', 'Event description for Community A', '2023-02-10 18:00:00', 'Event Location A'),
+  ('smpere7@gmail.com', 2, 'Event in Community B', 'Event description for Community B', '2023-03-15 17:30:00', 'Event Location B'),
+  ('smpere7@gmail.com', 2, 'Event in Community B', 'Event description for Community B', '2023-03-15 17:30:00', 'Event Location B'),
+  ('smpere7@gmail.com', 2, 'Event in Community B', 'Event description for Community B', '2023-03-15 17:30:00', 'Event Location B'),
+  ('smpere7@gmail.com', 2, 'Event in Community B', 'Event description for Community B', '2023-03-15 17:30:00', 'Event Location B'),
   ('smpere7@gmail.com', 2, 'Event in Community B', 'Event description for Community B', '2023-03-15 17:30:00', 'Event Location B');
