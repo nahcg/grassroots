777a5477ee2c1bdf471b82c44b40eeba25d8dade
nahcg <glor.chan@mail.utoronto.ca>
2023-11-01 17:42:08 -0400

    add event seed

--- backend/db/seeds/05_events.sql
+++ backend/db/seeds/05_events.sql
@@ -1,10 +1,12 @@
 INSERT INTO Events (community_id, title, description, date, location)
 VALUES
-  (1, 'Event in Community A', 'Event description for Community A', '2023-02-10', 'Toronto Community Centre'),
-  (1, 'Event in Community A', 'Event description for Community A', '2023-10-23', 'Toronto Park'),
-  (1, 'Event in Community A', 'Event description for Community A', '2023-10-22', 'Toronto School'),
-  (2, 'Event in Community B', 'Event description for Community B', '2023-10-30', 'Toronto Arena'),
-  (2, 'Event in Community B', 'Event description for Community B', '2023-10-15', 'Toronto Centre'),
-  (2, 'Event in Community B', 'Event description for Community B', '2023-10-30', 'Waterloo School'),
-  (3, 'Event in Community B', 'Event description for Community B', '2023-10-30', 'Guelph Park'),
-  (3, 'Event in Community B', 'Event description for Community B', '2023-10-30', 'Etobicoke Park');
+  (1, 'Town Hall Meeting', 'Event description for Community A', '2023-11-10', 'Toronto Community Centre'),
+  (1, 'Educational Workshop', 'Event description for Community A', '2023-11-23', 'Toronto School'),
+  (1, 'Petition Drive', 'Event description for Community A', '2023-11-22', 'Toronto Park'),
+  (2, 'Event in Community B', 'Event description for Community B', '2023-11-03', 'Toronto Arena'),
+  (2, 'Event in Community B', 'Event description for Community B', '2023-11-15', 'Toronto Centre'),
+  (2, 'Event in Community B', 'Event description for Community B', '2023-11-30', 'Waterloo School'),
+  (3, 'Event in Community C', 'Event description for Community B', '2023-11-10', 'Guelph Park'),
+  (3, 'Event in Community C', 'Event description for Community B', '2023-11-02', 'Etobicoke Park');
+  (4, 'Event in Community D', 'Event description for Community B', '2023-11-10', 'Guelph Park'),
+  (4, 'Event in Community D', 'Event description for Community B', '2023-11-02', 'Etobicoke Park');
