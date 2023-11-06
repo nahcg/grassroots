e1d16e37750ad73d0092d19508b9b3992e56fe85
nahcg <glor.chan@mail.utoronto.ca>
2023-11-05 19:02:41 -0500

    home page

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,70 +1,56 @@
 import React, { useState, useEffect } from "react";
-import Comment from "./Comment";
 
 const HomePosts = ({ posts, allPosts }) => {
   const [comments, setComments] = useState([]);
+  const [showYourPosts, setShowYourPosts] = useState(true);
 
   useEffect(() => {
-    const fetchCommentsForPosts = async () => {
-      try {
-        const post_ids = [...posts, ...allPosts].map((post) => post.post_id);
-        const commentsData = {};
-
-        await Promise.all(
-          post_ids.map(async (post_id) => {
-            const response = await fetch(`http://localhost:8080/home/comments/${post_id}`);
-            const data = await response.json();
-            commentsData[post_id] = data;
-          })
-        );
-
-        setComments(commentsData);
-      } catch (error) {
-        console.error("Error fetching comments:", error);
-      }
+    const fetchComments = async () => {
+      const postIds = [...(showYourPosts ? posts : allPosts)].map((post) => post.post_id);
+      const commentPromises = postIds.map((post_id) =>
+        fetch(`http://localhost:8080/home/comments/${post_id}`)
+          .then((response) => response.json())
+      );
+      const commentsData = await Promise.all(commentPromises);
+      setComments(commentsData);
     };
 
-    fetchCommentsForPosts();
-  }, [posts, allPosts]);
-
-  console.log("posts", posts);
-  console.log("allPosts", allPosts);
-  console.log("comments from HomePost", comments);
+    fetchComments();
+  }, [posts, allPosts, showYourPosts]);
 
   return (
     <div className="posts-container">
-      <h2>Your Posts: </h2>
-      {posts.map((post, index) => (
-        <div key={index} className="post-item">
-          <h3>{post.title}</h3>
-          <p>Date: {new Date(post.timestamp).toLocaleString()}</p>
-          <p>Author: {post.user_id}</p>
-          <p>{post.context}</p>
-          <div className="comments">
-            {comments[post.post_id] &&
-              comments[post.post_id].map((comment, index) => (
-                <Comment key={index} comment={comment} />
-              ))}
-          </div>
-        </div>
-      ))}
-      <div className="all-posts-box">
-        <h2>All Posts:</h2>
-        {allPosts.map((post, index) => (
+      <div className="toggle-buttons">
+        <button onClick={() => setShowYourPosts(true)}>Your Posts</button>
+        <button onClick={() => setShowYourPosts(false)}>All Posts</button>
+      </div>
+      {showYourPosts ? <h2>Your Posts: </h2> : <h2>All Posts: </h2>}
+      {(showYourPosts ? posts : allPosts).map((post, index) => {
+        const postComments = comments.find(
+          (comment) => comment[0] && comment[0].post_id === post.post_id
+        );
+
+        return (
           <div key={index} className="post-item">
             <h3>{post.title}</h3>
             <p>Date: {new Date(post.timestamp).toLocaleString()}</p>
             <p>Author: {post.user_id}</p>
             <p>{post.context}</p>
-            <div className="comments">
-              {comments[post.post_id] &&
-                comments[post.post_id].map((comment, index) => (
-                  <Comment key={index} comment={comment} />
-                ))}
-            </div>
+
+            {/* Render comments for this post */}
+            <h4>Comments:</h4>
+            <ul>
+              {postComments && postComments.length > 0 ? (
+                postComments.map((comment) => (
+                  <li key={comment.comment_id}>{comment.comment}</li>
+                ))
+              ) : (
+                <li>No comments yet</li>
+              )}
+            </ul>
           </div>
-        ))}
-      </div>
+        );
+      })}
     </div>
   );
 };
