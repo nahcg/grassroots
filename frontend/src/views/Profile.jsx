5deee225a81ffaa99a2ea7d56db517c054f8374f
Swathi Jayasree <swathij943@gmail.com>
2023-11-06 08:50:48 -0500

    fixing profile.css

--- frontend/src/views/Profile.jsx
+++ frontend/src/views/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import "../styles/profile.css";
+import "../styles/Profile.css";
 const Profile = () => {
 
 	const { user, isAuthenticated, isLoading } = useAuth0();
