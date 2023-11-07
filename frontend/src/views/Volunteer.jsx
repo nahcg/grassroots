2d8f6ec997bd3ed87e7020777a421a4059308c6b
Mithra Perera <mithraperera@hotmail.com>
2023-11-07 00:33:43 -0500

    fixed issue with reloading page and added page reload to show newly added volunteer position

--- frontend/src/views/Volunteer.jsx
+++ frontend/src/views/Volunteer.jsx
@@ -24,18 +24,11 @@ import {
 	FormHelperText,
 	Radio,
 } from "@mui/material";
-import SelectInput from "@mui/material/Select/SelectInput";
-
-// Imports for Date Picker
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 function Volunteer() {
 	const [activeContentIndex, setActiveContentIndex] = useState(0);
 	const [volunteerData, setVolunteerData] = useState([]);
 	const { user, isAuthenticated, isLoading } = useAuth0();
-	const userEmail = user.email;
 
 	// State for Create New Form Modal
 	const [open, setOpen] = useState(false);
@@ -67,7 +60,10 @@ function Volunteer() {
 					volunteers_needed: data.volunteers_needed.value,
 				},
 			})
-			.then(setOpen(false));
+			.then(() => {
+				setOpen(false);
+				window.location.reload(true);
+			});
 	};
 
 	// Style for Modal
@@ -84,132 +80,138 @@ function Volunteer() {
 		p: 4,
 	};
 
+	if (isLoading) {
+		return <div>Loading ...</div>;
+	}
+
 	return (
-		<div>
-			<Navbar />
-			<div className='volunteer-add-new_container'>
-				<button className='volunteer-add-new__button' onClick={handleOpen}>
-					Add New Position
-				</button>
-				<Modal
-					aria-labelledby='transition-modal-title'
-					aria-describedby='transition-modal-description'
-					open={open}
-					onClose={handleClose}
-					closeAfterTransition
-					slots={{ backdrop: Backdrop }}
-					slotProps={{
-						backdrop: {
-							timeout: 500,
-						},
-					}}
-				>
-					<Fade in={open}>
-						<Box sx={style}>
-							<Typography
-								id='transition-modal-title'
-								variant='h6'
-								component='h2'
-							>
-								Create New Volunteer Position
-							</Typography>
-							<Typography id='transition-modal-description' sx={{ mt: 2 }}>
-								Please fill out the below details to create your position:
-							</Typography>
-							<form onSubmit={handleSubmit}>
-								<Stack>
-									<Input name='name' placeholder='Volunteer Position Name' />
-									<Input name='description' placeholder='Description' />
-									<InputLabel>Location</InputLabel>
-									<Select name='location'>
-										<MenuItem value='Toronto'>Toronto</MenuItem>
-										<MenuItem value='Brampton'> Brampton</MenuItem>
-										<MenuItem value='Mississauga'> Mississauga</MenuItem>
-										<MenuItem value='Vaughan'> Vaughan</MenuItem>
-										<MenuItem value='Richmond Hill'> Richmond Hill</MenuItem>
-										<MenuItem value='Scarborough'> Scarborough</MenuItem>
-										<MenuItem value='North York'> North York</MenuItem>
-									</Select>
-									<InputLabel>Cause</InputLabel>
-									<Select name='cause'>
-										<MenuItem value='1'>Political</MenuItem>
-										<MenuItem value='2'>Environmental</MenuItem>
-										<MenuItem value='3'>Social</MenuItem>
-									</Select>
-									<InputLabel>Start Date</InputLabel>
-									<input type='date' name='start_date' />
-									<InputLabel>End Date</InputLabel>
-									<input type='date' name='end_date' />
-									<Input
-										name='volunteers_needed'
-										placeholder='# of Vulunteers'
-									/>
-								</Stack>
-								<Button type='submit'>Submit</Button>
-							</form>
-						</Box>
-					</Fade>
-				</Modal>
-			</div>
-			<div className='tabs'>
-				<menu>
-					<button
-						className={activeContentIndex === 0 ? "active" : ""}
-						onClick={() => setActiveContentIndex(0)}
-					>
-						All
+		isAuthenticated && (
+			<div>
+				<Navbar />
+				<div className='volunteer-add-new_container'>
+					<button className='volunteer-add-new__button' onClick={handleOpen}>
+						Add New Position
 					</button>
-					<button
-						className={activeContentIndex === 1 ? "active" : ""}
-						onClick={() => setActiveContentIndex(1)}
+					<Modal
+						aria-labelledby='transition-modal-title'
+						aria-describedby='transition-modal-description'
+						open={open}
+						onClose={handleClose}
+						closeAfterTransition
+						slots={{ backdrop: Backdrop }}
+						slotProps={{
+							backdrop: {
+								timeout: 500,
+							},
+						}}
 					>
-						Open
-					</button>
-					<button
-						className={activeContentIndex === 2 ? "active" : ""}
-						onClick={() => setActiveContentIndex(2)}
-					>
-						In Progress
-					</button>
-					<button
-						className={activeContentIndex === 3 ? "active" : ""}
-						onClick={() => setActiveContentIndex(3)}
-					>
-						Completed
-					</button>
-				</menu>
-				<div className='tab-content'>
-					{/* <ul>{}</ul> */}
-					{activeContentIndex === 0 && (
-						<SkillSwapList volunteerData={volunteerData} user={userEmail} />
-					)}
-					{activeContentIndex === 1 && (
-						<SkillSwapList
-							volunteerData={volunteerData.filter(
-								(obj) => obj.status === "Open"
-							)}
-							user={userEmail}
-						/>
-					)}
-					{activeContentIndex === 2 && (
-						<SkillSwapList
-							volunteerData={volunteerData.filter(
-								(obj) => obj.status === "In Progress"
-							)}
-							user={userEmail}
-						/>
-					)}
-					{activeContentIndex === 3 && (
-						<SkillSwapList
-							volunteerData={volunteerData.filter(
-								(obj) => obj.status === "Completed"
-							)}
-							user={userEmail}
-						/>
-					)}
+						<Fade in={open}>
+							<Box sx={style}>
+								<Typography
+									id='transition-modal-title'
+									variant='h6'
+									component='h2'
+								>
+									Create New Volunteer Position
+								</Typography>
+								<Typography id='transition-modal-description' sx={{ mt: 2 }}>
+									Please fill out the below details to create your position:
+								</Typography>
+								<form onSubmit={handleSubmit}>
+									<Stack>
+										<Input name='name' placeholder='Volunteer Position Name' />
+										<Input name='description' placeholder='Description' />
+										<InputLabel>Location</InputLabel>
+										<Select name='location'>
+											<MenuItem value='Toronto'>Toronto</MenuItem>
+											<MenuItem value='Brampton'> Brampton</MenuItem>
+											<MenuItem value='Mississauga'> Mississauga</MenuItem>
+											<MenuItem value='Vaughan'> Vaughan</MenuItem>
+											<MenuItem value='Richmond Hill'> Richmond Hill</MenuItem>
+											<MenuItem value='Scarborough'> Scarborough</MenuItem>
+											<MenuItem value='North York'> North York</MenuItem>
+										</Select>
+										<InputLabel>Cause</InputLabel>
+										<Select name='cause'>
+											<MenuItem value='1'>Political</MenuItem>
+											<MenuItem value='2'>Environmental</MenuItem>
+											<MenuItem value='3'>Social</MenuItem>
+										</Select>
+										<InputLabel>Start Date</InputLabel>
+										<input type='date' name='start_date' />
+										<InputLabel>End Date</InputLabel>
+										<input type='date' name='end_date' />
+										<Input
+											name='volunteers_needed'
+											placeholder='# of Vulunteers'
+										/>
+									</Stack>
+									<Button type='submit'>Submit</Button>
+								</form>
+							</Box>
+						</Fade>
+					</Modal>
+				</div>
+				<div className='tabs'>
+					<menu>
+						<button
+							className={activeContentIndex === 0 ? "active" : ""}
+							onClick={() => setActiveContentIndex(0)}
+						>
+							All
+						</button>
+						<button
+							className={activeContentIndex === 1 ? "active" : ""}
+							onClick={() => setActiveContentIndex(1)}
+						>
+							Open
+						</button>
+						<button
+							className={activeContentIndex === 2 ? "active" : ""}
+							onClick={() => setActiveContentIndex(2)}
+						>
+							In Progress
+						</button>
+						<button
+							className={activeContentIndex === 3 ? "active" : ""}
+							onClick={() => setActiveContentIndex(3)}
+						>
+							Completed
+						</button>
+					</menu>
+					<div className='tab-content'>
+						{/* <ul>{}</ul> */}
+						{activeContentIndex === 0 && (
+							<SkillSwapList volunteerData={volunteerData} user={user.email} />
+						)}
+						{activeContentIndex === 1 && (
+							<SkillSwapList
+								volunteerData={volunteerData.filter(
+									(obj) => obj.status === "Open"
+								)}
+								user={user.email}
+							/>
+						)}
+						{activeContentIndex === 2 && (
+							<SkillSwapList
+								volunteerData={volunteerData.filter(
+									(obj) => obj.status === "In Progress"
+								)}
+								user={user.email}
+							/>
+						)}
+						{activeContentIndex === 3 && (
+							<SkillSwapList
+								volunteerData={volunteerData.filter(
+									(obj) => obj.status === "Completed"
+								)}
+								user={user.email}
+							/>
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
+		)
 	);
 }
 
