7d37867118f8ae840444bce2ac4f8ca7909903c4
Mithra Perera <mithraperera@hotmail.com>
2023-11-07 22:49:04 -0500

    use env variables for auth0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,19 +7,19 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 axios.defaults.baseURL = process.env.REACT_APP_BASEURL || 'http://localhost:8080';
 
 root.render(
   <BrowserRouter>
     <Auth0Provider
-      domain="dev-ehtvsgfhrdqhuole.us.auth0.com"
-      clientId="jjAaGEPKXel2iltw3mO5fgRJWdFzzdjz"
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         // redirect_uri: window.location.origin,
-        redirect_uri: "http://localhost:3000/home",
+        redirect_uri: "/home",
       }}
     >
       <App />
