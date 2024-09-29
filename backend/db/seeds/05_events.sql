7c78a118b0dcfbc286a350999121e20a00713a9a
nahcg <glor.chan@mail.utoronto.ca>
2024-09-28 21:18:46 -0400

    update event dates in seeds

--- backend/db/seeds/05_events.sql
+++ backend/db/seeds/05_events.sql
@@ -1,7 +1,7 @@
 INSERT INTO Events (community_id, title, description, date, location)
 VALUES
   (1, 'Town Hall Meeting', 'Event description for Community A', '2024-09-10', 'Toronto Community Centre'),
-  (1, 'Educational Workshop', 'Event description for Community A', '2024-05-23', 'Toronto School'),
+  (1, 'Educational Workshop', 'Event description for Community A', '2024-09-23', 'Toronto School'),
   (1, 'Petition Drive', 'Event description for Community A', '2024-09-12', 'Toronto Park'),
   (2, 'Event in Community B', 'Event description for Community B', '2024-09-03', 'Toronto Arena'),
   (2, 'Event in Community B', 'Event description for Community B', '2024-09-15', 'Toronto Centre'),
@@ -9,7 +9,7 @@ VALUES
   (3, 'Event in Community C', 'Event description for Community C', '2024-09-10', 'Guelph Park'),
   (3, 'Event in Community C', 'Event description for Community C', '2024-09-02', 'Etobicoke Park'),
   (4, 'Event in Community D', 'Event description for Community D', '2024-09-16', 'Guelph Park'),
-  (4, 'Event in Community D', 'Event description for Community D', '2024-05-11', 'Guelph Park'),
+  (4, 'Event in Community D', 'Event description for Community D', '2024-09-11', 'Guelph Park'),
   (4, 'Event in Community D', 'Event description for Community D', '2024-05-19', 'Guelph Park'),
   (5, 'Event in Community E', 'Event description for Community E', '2024-05-26', 'Guelph Park'),
   (6, 'Event in Community F', 'Event description for Community F', '2024-05-22', 'Etobicoke Park');
